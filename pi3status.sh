316a2a844faf9bdbc2102c2e18d66ba0913026d1
P Paul Jonathan <ppauljonathan@gmail.com>
2022-09-15 21:29:46 +0530

    edited pi3

--- pi3status.sh
+++ pi3status.sh
@@ -3,7 +3,7 @@
 i3status | while :
 do
 	read line
-	echo "| BRT "$(~/brightness.sh) "% | VOL "$(pamixer --get-volume-human)" | $line" || exit 1
+	echo "| BRT "$(~/brightness.sh)"% | VOL "$(pamixer --get-volume-human)" | $line" || exit 1
 done
 
 
