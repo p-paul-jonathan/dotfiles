631c534cada46392a5055b049852b461f8ba10e8
P Paul Jonathan <ppauljonathan@gmail.com>
2022-11-30 18:00:58 +0530

    bug in touchpad_settings solved

--- shell_scripts/set_touchpad_settings.sh
+++ shell_scripts/set_touchpad_settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 2
 id=$(xinput list | grep SynPS/2 | awk '{print $6}' | cut -d = -f 2)
-tap_id=$(xinput list-props 13 | grep 'libinput Tapping Enabled (' | cut -d '(' -f 2 | cut -d ')' -f 1)
-speed_id=$(xinput list-props 13 | grep 'libinput Accel Speed (' | cut -d '(' -f 2 | cut -d ')' -f 1)
+tap_id=$(xinput list-props $id | grep 'libinput Tapping Enabled (' | cut -d '(' -f 2 | cut -d ')' -f 1)
+speed_id=$(xinput list-props $id | grep 'libinput Accel Speed (' | cut -d '(' -f 2 | cut -d ')' -f 1)
 xinput set-prop $id $tap_id 1
 xinput set-prop $id $speed_id 0.4
