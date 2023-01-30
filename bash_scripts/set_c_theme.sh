8dd175552537f921516fb244a5820606e0c82627
P Paul Jonathan <ppauljonathan@gmail.com>
2023-01-30 17:23:22 +0530

    major overhaul

---
+++ bash_scripts/set_c_theme.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Theme to toggle between light mode and dark mode
+
+if [ ! -f ~/.paul_theme ]; then
+	echo 0 > ~/.paul_theme
+fi
+
+
+
+curr_col=$(cat ~/.paul_theme)
+curr_theme=$(readlink -f ~/.config/i3/config | cut -d / -f 6 | cut -d - -f 1)
+
+if [ "$1" = '-t' ]; then
+	if [ $curr_col -eq 0 ]; then
+		echo 1 > ~/.paul_theme
+	else
+		echo 0 > ~/.paul_theme
+	fi
+	exit 0
+fi
+
+case $curr_theme in
+	purple)
+		if [ $curr_col -eq 0 ]; then
+			feh --bg-fill ~/backgrounds/purple.jpg
+		else
+			feh --bg-fill ~/backgrounds/purple-light.png
+		fi
+		;;
+	green)
+		if [ $curr_col -eq 0 ]; then
+			feh --bg-fill ~/backgrounds/green.jpg
+		else
+			feh --bg-fill ~/backgrounds/green-light.jpg
+		fi
+		;;
+	blue)
+		if [ $curr_col -eq 0 ]; then
+			feh --bg-fill ~/backgrounds/blue.png
+		else
+			feh --bg-fill ~/backgrounds/blue-light.jpg
+		fi
+		;;
+	*)
+		if [ $curr_col -eq 0 ]; then
+			feh --bg-fill ~/backgrounds/nord.jpg
+		else
+			feh --bg-fill ~/backgrounds/nord-light.jpg
+		fi
+		;;
+esac
+
