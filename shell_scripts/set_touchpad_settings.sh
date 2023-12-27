4dcf3757c7d690e50a5172f7ed8588b8b059d30b
P Paul Jonathan <ppauljonathan@gmail.com>
2023-12-27 11:31:11 +0530

    27-12-2023

--- shell_scripts/set_touchpad_settings.sh
+++ shell_scripts/set_touchpad_settings.sh
@@ -3,5 +3,8 @@ sleep 2
 id=$(xinput list | grep SynPS/2 | awk '{print $6}' | cut -d = -f 2)
 tap_id=$(xinput list-props $id | grep 'libinput Tapping Enabled (' | cut -d '(' -f 2 | cut -d ')' -f 1)
 speed_id=$(xinput list-props $id | grep 'libinput Accel Speed (' | cut -d '(' -f 2 | cut -d ')' -f 1)
+natural_scrolling_id=$(xinput list-props $id | grep 'libinput Natural Scrolling Enabled (' | cut -d '(' -f 2 | cut -d ')' -f 1)
+
 xinput set-prop $id $tap_id 1
 xinput set-prop $id $speed_id 0.4
+xinput set-prop $id $natural_scrolling_id 1
