75e5741e93573c56b49708ba5e168762a4a1b246
P Paul Jonathan <ppauljonathan@gmail.com>
2025-03-31 16:41:10 +0530

    add neogit plugin for nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -127,7 +127,18 @@ require("lazy").setup({
     end
   },
 
+  
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
+  },
+
   -- Comments
-  { 'numToStr/Comment.nvim', }
+  { 'numToStr/Comment.nvim', },
 })
 
