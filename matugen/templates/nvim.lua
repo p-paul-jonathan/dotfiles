51d4f829b85f823b72d1d8a5226cd88ffb41a023
P Paul Jonathan <ppauljonathan@gmail.com>
2026-09-26 20:45:51 +0530

    add matugen themes from niri dms

---
+++ matugen/templates/nvim.lua
@@ -0,0 +1,25 @@
+return {
+  bg = "{{colors.surface.default.hex}}",
+  bg_alt = "{{colors.surface_container.default.hex}}",
+  bg_float = "{{colors.surface_container_high.default.hex}}",
+
+  fg = "{{colors.on_surface.default.hex}}",
+  fg_muted = "{{colors.on_surface_variant.default.hex}}",
+
+  primary = "{{colors.primary.default.hex}}",
+  primary_container = "{{colors.primary_container.default.hex}}",
+
+  secondary = "{{colors.secondary.default.hex}}",
+  tertiary = "{{colors.tertiary.default.hex}}",
+
+  error = "{{colors.error.default.hex}}",
+
+  -- Semantic colors
+  red = "{{colors.error.default.hex}}",
+  orange = "{{colors.tertiary.default.hex}}",
+  yellow = "{{colors.secondary.default.hex}}",
+  green = "{{colors.primary.default.hex}}",
+  cyan = "{{colors.secondary.default.hex}}",
+  blue = "{{colors.primary.default.hex}}",
+  purple = "{{colors.tertiary.default.hex}}",
+}
